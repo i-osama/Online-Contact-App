a1924dd0727b02f81431ca660f8b2e374d0c1c7e
Osama Bin Hashim <zeeshan2k01@gmail.com>
2022-12-04 13:57:00 +0600

    Add page added

--- app/src/main/java/com/zeeshan_s/firebasecontactapp/Activities/GetStarted.java
+++ app/src/main/java/com/zeeshan_s/firebasecontactapp/Activities/GetStarted.java
@@ -1,7 +1,10 @@
 package com.zeeshan_s.firebasecontactapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.zeeshan_s.firebasecontactapp.R;
@@ -12,5 +15,25 @@ public class GetStarted extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        AppCompatButton startBtn = findViewById(R.id.getStartedBtn);
+
+        SharedPreferences preferences = getSharedPreferences("getStartedButtonClicked", MODE_PRIVATE);
+
+        boolean isClicked = preferences.getBoolean("userClicked", false);
+        if (isClicked){
+            startActivity(new Intent(GetStarted.this, MainActivity.class));
+        }
+
+        startBtn.setOnClickListener(view -> {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("userClicked",true);
+            editor.apply();
+
+            Intent intent = new Intent(GetStarted.this, MainActivity.class);
+            startActivity(intent);
+
+        });
+
     }
 }
