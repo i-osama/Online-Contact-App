71cc41aeafd715e46fd8434dcd8b82c8f301d3bb
Osama Bin Hashim <zeeshan2k01@gmail.com>
2022-12-04 12:13:51 +0600

    Initial commit

---
+++ app/src/main/java/com/zeeshan_s/firebasecontactapp/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zeeshan_s.firebasecontactapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zeeshan_s.firebasecontactapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
